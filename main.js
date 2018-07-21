d7f794772616632649db35882dec738cce40b678
dex <dextronox@users.noreply.github.com>
2018-07-21 16:22:01 +0800

    Added squirrel suppport

--- main.js
+++ main.js
@@ -5,11 +5,6 @@ const path = require('path')
 const url = require('url')
 const log = require('electron-log')
 const fs = require('fs')
-require('update-electron-app')({
-    repo: 'dextronox/viper',
-    updateInterval: '5 minutes',
-    logger: require('electron-log')
-})
 let loginWindow, connectWindow
 
 // Same as for console transport
@@ -72,7 +67,10 @@ function createAlertWindow () {
         alertWindow = null
     })
 }
-app.on('ready', createLoginWindow)
+
+app.on('ready', () => {
+    createLoginWindow()
+})
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
         app.quit()
@@ -102,3 +100,72 @@ exports.alert = () => {
     createAlertWindow()
     connectWindow.close()
 }
+
+//Squirrel
+// this should be placed at top of main.js to handle setup events quickly
+if (handleSquirrelEvent()) {
+    // squirrel event handled and app will exit in 1000ms, so don't do anything else
+    return;
+  }
+  
+  function handleSquirrelEvent() {
+    if (process.argv.length === 1) {
+      return false;
+    }
+  
+    const ChildProcess = require('child_process');
+    const path = require('path');
+  
+    const appFolder = path.resolve(process.execPath, '..');
+    const rootAtomFolder = path.resolve(appFolder, '..');
+    const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+    const exeName = path.basename(process.execPath);
+  
+    const spawn = function(command, args) {
+      let spawnedProcess, error;
+  
+      try {
+        spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
+      } catch (error) {}
+  
+      return spawnedProcess;
+    };
+  
+    const spawnUpdate = function(args) {
+      return spawn(updateDotExe, args);
+    };
+  
+    const squirrelEvent = process.argv[1];
+    switch (squirrelEvent) {
+      case '--squirrel-install':
+      case '--squirrel-updated':
+        // Optionally do things such as:
+        // - Add your .exe to the PATH
+        // - Write to the registry for things like file associations and
+        //   explorer context menus
+  
+        // Install desktop and start menu shortcuts
+        spawnUpdate(['--createShortcut', exeName]);
+  
+        setTimeout(app.quit, 1000);
+        return true;
+  
+      case '--squirrel-uninstall':
+        // Undo anything you did in the --squirrel-install and
+        // --squirrel-updated handlers
+  
+        // Remove desktop and start menu shortcuts
+        spawnUpdate(['--removeShortcut', exeName]);
+  
+        setTimeout(app.quit, 1000);
+        return true;
+  
+      case '--squirrel-obsolete':
+        // This is called on the outgoing version of your app before
+        // we update to the new version - it's the opposite of
+        // --squirrel-updated
+  
+        app.quit();
+        return true;
+    }
+  };
